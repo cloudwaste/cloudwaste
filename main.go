c28438fbf8ee6a9f73800578261f26d4ce3e12fe
Timothy Myers <timmyers09@gmail.com>
2020-05-02 20:11:22 -0600

    feat(cli): initial cli work

--- main.go
+++ main.go
@@ -1,31 +1,28 @@
 package main
 
-import (
-	"context"
-	"log"
-
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	ec2Waste "github.com/timmyers/cloudwaste/pkg/aws/ec2"
-)
+import "github.com/timmyers/cloudwaste/cmd"
 
 func main() {
-	sess := session.Must(session.NewSession())
-
-	// unusedAddresses, err := ec2.GetUnusedElasticIPAddresses(sess)
-	// if err != nil {
-	// 	return
-	// }
-	// log.Printf("%d unused addresses", len(unusedAddresses))
-	// _ = ec2.GetUnusedElasticIPAddressPrice(sess)
-
-	client := &ec2Waste.EC2{
-		Client: ec2.New(sess),
-	}
-
-	unusedGateways, err := client.GetUnusedNATGateways(context.TODO())
-	if err != nil {
-		return
-	}
-	log.Printf("%d unused gateways", len(unusedGateways))
+	cmd.Execute()
 }
+
+// func main() {
+// 	sess := session.Must(session.NewSession())
+
+// 	// unusedAddresses, err := ec2.GetUnusedElasticIPAddresses(sess)
+// 	// if err != nil {
+// 	// 	return
+// 	// }
+// 	// log.Printf("%d unused addresses", len(unusedAddresses))
+// 	// _ = ec2.GetUnusedElasticIPAddressPrice(sess)
+
+// 	client := &ec2Waste.EC2{
+// 		Client: ec2.New(sess),
+// 	}
+
+// 	unusedGateways, err := client.GetUnusedNATGateways(context.TODO())
+// 	if err != nil {
+// 		return
+// 	}
+// 	log.Printf("%d unused gateways", len(unusedGateways))
+// }
--- pkg/aws/aws.go
+++ pkg/aws/aws.go
@@ -1,15 +1,44 @@
 package aws
 
 import (
-	"github.com/aws/aws-sdk-go/aws/endpoints"
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+
+	ec2Waste "github.com/timmyers/cloudwaste/pkg/aws/ec2"
 )
 
-type Price struct {
-	Unit string
-	Rate string
+type AWSResource interface {
+	Type() string
+	ID() string
 }
 
-var RegionLongNames = map[string]string{
-	endpoints.UsEast1RegionID: "US East (N. Virginia)",
-	endpoints.UsEast2RegionID: "US East (Ohio)",
+func AnalyzeWaste() {
+	sess := session.Must(session.NewSession())
+
+	client := &ec2Waste.EC2{
+		Client: ec2.New(sess, aws.NewConfig().WithRegion("us-east-1")),
+	}
+
+	var unusedResources []AWSResource
+	unusedAddresses, err := client.GetUnusedElasticIPAddresses(context.TODO())
+	if err == nil {
+		for _, address := range unusedAddresses {
+			unusedResources = append(unusedResources, address)
+		}
+	}
+
+	unusedGateways, err := client.GetUnusedNATGateways(context.TODO())
+	if err != nil {
+		for _, gateway := range unusedGateways {
+			unusedResources = append(unusedResources, gateway)
+		}
+	}
+
+	for _, r := range unusedResources {
+		fmt.Printf("%s - %s\n", r.Type(), r.ID())
+	}
 }
